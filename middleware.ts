187982f7b989e41079f4865e7ebc65524baf3215
LarbeartoW <larbeartow@gmail.com>
2024-03-13 00:35:28 -0700

    middleware update

--- middleware.ts
+++ middleware.ts
@@ -1,43 +1,56 @@
-import { getToken } from 'next-auth/jwt'
-import { withAuth } from 'next-auth/middleware'
-import { NextResponse } from 'next/server'
+// import { withAuth } from 'next-auth/middleware'
 
-export default withAuth(
-  async function middleware(req) {
-    const token = await getToken({ req })
-    const isAuth = !!token
-    const isAuthPage =
-      req.nextUrl.pathname.startsWith('/login') || req.nextUrl.pathname.startsWith('/register')
 
-    if (isAuthPage) {
-      if (isAuth) {
-        return NextResponse.redirect(new URL('/dashboard', req.url))
-      }
+// Or like this if you need to do something here.
+// export default auth((req) => {
+//   console.log(req.auth) //  { session: { user: { ... } } }
+// })
 
-      return null
-    }
-
-    if (!isAuth) {
-      let from = req.nextUrl.pathname
-      if (req.nextUrl.search) {
-        from += req.nextUrl.search
-      }
-
-      return NextResponse.redirect(new URL(`/login?from=${encodeURIComponent(from)}`, req.url))
-    }
-  },
-  {
-    callbacks: {
-      async authorized() {
-        // This is a work-around for handling redirect on auth pages.
-        // We return true here so that the middleware function above
-        // is always called.
-        return true
-      }
-    }
-  }
-)
-
+// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
-  matcher: ['/dashboard/:path*', '/home/:path*', '/login', '/register']
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 }
+
+// import { getToken } from 'next-auth/jwt'
+// import { withAuth } from 'next-auth/middleware'
+// import { NextResponse } from 'next/server'
+
+// export default withAuth(
+//   async function middleware(req) {
+//     const token = await getToken({ req })
+//     const isAuth = !!token
+//     const isAuthPage =
+//       req.nextUrl.pathname.startsWith('/login') || req.nextUrl.pathname.startsWith('/register')
+
+//     if (isAuthPage) {
+//       if (isAuth) {
+//         return NextResponse.redirect(new URL('/dashboard', req.url))
+//       }
+
+//       return null
+//     }
+
+//     if (!isAuth) {
+//       let from = req.nextUrl.pathname
+//       if (req.nextUrl.search) {
+//         from += req.nextUrl.search
+//       }
+
+//       return NextResponse.redirect(new URL(`/login?from=${encodeURIComponent(from)}`, req.url))
+//     }
+//   },
+//   {
+//     callbacks: {
+//       async authorized() {
+//         // This is a work-around for handling redirect on auth pages.
+//         // We return true here so that the middleware function above
+//         // is always called.
+//         return true
+//       }
+//     }
+//   }
+// )
+
+// export const config = {
+//   matcher: ['/dashboard/:path*', '/home/:path*', '/login', '/register']
+// }
